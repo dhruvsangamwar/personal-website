2813477125f8f60a4c1f2145b6a7b6c9dc1ee47c
Dhruv Sangamwar <dsangamwar@gmail.com>
2024-12-24 17:55:08 -0800

    redid the entire thing

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Dhruv Sangamwar
+      </h1>
+      
+      <p className="mb-4">
+        I am currently working as a software engineer at 
+        <span className="font-bold"> PNI Sensor </span>, 
+        where I develop software supporting alternative positioning, navigation, and timing sensors. 
+        Recently, I have been working with Android (Kotlin), Rust, and C. My interests include databases, 
+        distributed systems, computer networks, and natural language processing.
+      </p>
+      
+      <p className="mb-4">
+        I studied Computer Science at 
+        <span className="font-bold"> University of California, Davis </span>, 
+        where I had the opportunity to teach a class on algorithms and competitive programming 
+        and help manage Neurotech@Davis. I also contributed to research studying governance in Apache Open Source projects.
+      </p>
+      
+      <p className="mb-4">
+        When I’m not at my computer, you can find me hiking, traveling, or bouldering.
+      </p>
+    </section>
+  );
+}
